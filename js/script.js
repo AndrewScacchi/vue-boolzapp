80da5ef4e0ba1e9b5285c64fd78d2ff761d0e6de
andrea scacchi <andrea.scacchi@live.com>
2022-05-31 13:58:14 +0200

    Milestone2 midCommit02 added msg dinamically on click. todo add class msg-user on certain msg based on obj property

--- js/script.js
+++ js/script.js
@@ -34,37 +34,103 @@ const boolzApp = new Vue({
                 lastOn: "15:30", 
                 msgHistory: [
                     {
-                        msg: "placeholder text1",
+                        msg: "placeholder text Bobette_01",
                         owner : "contact"
                     },
-                     {
-                        msg: "placeholder text2",
+                    {
+                        msg: "placeholder text Bobette_02",
                         owner : "me"
-                     },
-                     {
-                        msg: "placeholder text3",
+                    },
+                    {
+                        msg: "placeholder text Bobette_03",
                         owner : "contact"
-                     },
-                     {
-                        msg: "placeholder text4",
+                    },
+                    {
+                        msg: "placeholder text Bobette_04",
                         owner : "me"
-                     }
+                    }
                  ]
             },
             {
                 name: "Andrew McNamara",
                 img: "contact_02.jpg",
                 lastOn: "12:15",
+                msgHistory: [
+                    {
+                        msg: "placeholder text McNamara_01",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text McNamara_02",
+                        owner : "me"
+                    },
+                    {
+                        msg: "placeholder tex tMcNamara_03",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text McNamara_04",
+                        owner : "me"
+                    }
+                 ]
             },
             {
                 name: "Sarah Garfield",
                 img: "contact_03.jpg",
                 lastOn: "11:10",
+                msgHistory: [
+                    {
+                        msg: "placeholder text Garfield_01",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text Garfield_02",
+                        owner : "me"
+                    },
+                    {
+                        msg: "placeholder text Garfield_03",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text Garfield_04",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text Garfield_05",
+                        owner : "me"
+                    },
+                ]
             },
             {
                 name: "Jim Nakamura",
                 img: "contact_04.jpg",
                 lastOn: "7:30",
+                msgHistory: [
+                    {
+                        msg: "placeholder text Nakamura_01",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text Nakamura_02",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text Nakamura_03",
+                        owner : "me"
+                    },
+                    {
+                        msg: "placeholder text Nakamura_04",
+                        owner : "me"
+                    },
+                    {
+                        msg: "placeholder text Nakamura_05",
+                        owner : "contact"
+                    },
+                    {
+                        msg: "placeholder text Nakamura_06",
+                        owner : "me"
+                    },
+                ]
             },  
         ],
 
@@ -73,7 +139,9 @@ const boolzApp = new Vue({
         onClick(activeContact){
             this.activeContact = activeContact;
             // document.querySelector(".content").innerHTML =  "{{contacts[activeContact].name}}";
-    
+        },
+        printMsg(){
+
         }
 
     },
